c5d6afa6fdd2fc6478e765575ee5f7da29ddc2a5
Prakhar Srivastav <prakhar1989@gmail.com>
2013-11-05 18:41:06 +0300

    Added json sampling and colored markers

--- geojson.py
+++ geojson.py
@@ -2,35 +2,36 @@ import json
 import requests
 
 def getData(url):
-	r = requests.get(url)
-	return r.json()
+    r = requests.get(url)
+    return r.json()
 
-def convertData(data, msymbol="restaurant", msize="small"):
-	data_dict = []
-	for d in data:  # test for first 10
-		if d.get('longitude') and d.get("latitude"):
-			data_dict.append({
-				"type": "Feature",
-				"geometry": {
-					"type": "Point",
-					"coordinates": [float(d["longitude"]),
-									float(d["latitude"])]
-				},
-				"properties": {
-					"name": d.get("applicant", ""),
-					"marker-symbol": msymbol,
-					"marker-size": msize,
-					"fooditems": d.get('fooditems', ""),
-					"address": d.get("address", "")
-				}
-			})
-	return data_dict
+def convertData(data, msymbol="restaurant", msize="medium"):
+    data_dict = []
+    for d in data:  # test for first 10
+        if d.get('longitude') and d.get("latitude"):
+            data_dict.append({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [float(d["longitude"]),
+                                    float(d["latitude"])]
+                },
+                "properties": {
+                    "name": d.get("applicant", ""),
+                    "marker-symbol": msymbol,
+                    "marker-size": msize,
+                    "marker-color": "#CC0033",
+                    "fooditems": d.get('fooditems', ""),
+                    "address": d.get("address", "")
+                }
+            })
+    return data_dict
 
 def writeToFile(data, filename="data.geojson"):
-	template = { "type": "FeatureCollections", "features": data }
-	with open(filename, "w") as f:
-		json.dump(template, f, indent=2)
+    template = { "type": "FeatureCollections", "features": data }
+    with open(filename, "w") as f:
+        json.dump(template, f, indent=2)
 
 if __name__ == "__main__":
-	data = getData("http://data.sfgov.org/resource/rqzj-sfat.json")
-	writeToFile(convertData(data[:40]), filename="myfile.geojson")
+    data = getData("http://data.sfgov.org/resource/rqzj-sfat.json")
+    writeToFile(convertData(data[:350]), filename="geojson.json")
--- static/js/app.js
+++ static/js/app.js
@@ -4,28 +4,30 @@
 window.App = {
     Models: {},
     Collections: {},
-    Views: {}
+    Views: {},
+    Markers: []
 };
 
+//helpers
 window.template = function(id) {
     return _.template( $(id).html() );
+}
+
+window.getgeoJSONSample = function(geoJson, n) {
+    return { 
+        type: "FeatureCollections" ,
+        features: _.sample(geoJson.features, n) 
+    }
 }
 
 App.Map = L.mapbox.map('map', 'prakhar.map-xt3ojyos');
 
 App.Models.Truck = Backbone.Model.extend({
-    defaults: {
-      geometry: { type: "Point", coordinates: [0, 0] }, 
-      type: "Feature", 
-      properties: {
-        fooditems: "None", 
-        address: "NA", 
-        title: "New Truck", 
-        "marker-symbol": "restaurant", "marker-size": "small"
-      }
+    getLatLng: function() { 
+        return this.get('geometry')["coordinates"].reverse()
     },
-    getCoordinates: function() {
-        return this.get('geometry')["coordinates"].reverse();
+    showOnMap: function() { 
+        //openPopup App.Markers[i].openPopup()
     }
 });
 
@@ -35,13 +37,15 @@ App.Collections.Trucks = Backbone.Collection.extend({
 
 App.Views.Truck = Backbone.View.extend({
     tagName: "li",
+    className: "truckCard",
     template: template('#truckTemplate'),
     events: {
         'click': "focusMap"
     },
     focusMap: function() {
-        var coordinates = this.model.getCoordinates();
-        App.Map.setView(coordinates, 16);
+        //var coordinates = this.model.getCoordinates();
+        App.Map.setView(this.model.getLatLng(), 14);
+        this.model.showOnMap();
     },
     render: function() {
         this.$el.html(this.template(this.model.toJSON()));
@@ -69,20 +73,27 @@ App.Views.Tooltip = Backbone.View.extend({
     }
 });
 
-// adding custom popups
+App.Trucks = new App.Collections.Trucks();
+
 App.Map.markerLayer.on("layeradd", function(e){
     var marker = e.layer,
         feature = marker.feature;
+    
+    // save the markers
+    App.Markers.push(marker);
+
+    // set the tooltip content
     var tooltip = new App.Models.Truck(feature);
     var popupContent = new App.Views.Tooltip({ model: tooltip }).render().el;
     marker.bindPopup(popupContent, { closeButton: true });
 });
 
-// setting up geoJSON
-App.Map.markerLayer.setGeoJSON(geoJson);
+// setting up geoJSON - select a random 30 sample, which can be ratings driven later on
+window.jsonsample = getgeoJSONSample(geoJson, 30);
+App.Map.markerLayer.setGeoJSON(jsonsample);
 
 // Hooking up data
-var trucks = new App.Collections.Trucks(geoJson.features);
+var trucks = new App.Collections.Trucks(jsonsample.features);
 var trucksView = new App.Views.Trucks({ collection: trucks });
 
 // rendering views
