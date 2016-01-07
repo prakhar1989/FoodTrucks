18b295b255fde8b109c023d036adc8161d3eafd8
Prakhar Srivastav <prakhar1989@gmail.com>
2016-01-06 20:38:48 -0500

    Added code for results processing and some UI progress

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from elasticsearch import Elasticsearch
 from flask import Flask, jsonify, request, render_template
-from urllib3 import exceptions
-
-from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
 app = Flask(__name__)
@@ -23,15 +20,63 @@ def test_es():
         resp["msg"] = "Unable to reach ES"
     return jsonify(resp)
 
+def formatFooditems(string):
+    items = [x.strip().lower() for x in string.split(":")]
+    return items
+
 @app.route('/search')
 def search():
     key = request.args.get('q')
     if not key:
-        return "nothing found"
-    res = es.search(index="sfdata", body={"query":
-      {"match": {"fooditems": key}}
+        return jsonify({
+            "status": "failure",
+            "msg": "Please provide a query"
+        })
+    try:
+        res = es.search(
+                index="sfdata",
+                body={
+                    "query": {"match": {"fooditems": key}},
+                    "size": 750 # TODO: shouldn't be hardcoded
+              })
+    except Exception as e:
+        return jsonify({
+            "status": "failure",
+            "msg": "error in reaching elasticsearch"
+        })
+    # filtering results
+    vendors = set([x["_source"]["applicant"] for x in res["hits"]["hits"]])
+    temp = {v: [] for v in vendors}
+    fooditems = {v: "" for v in vendors}
+    for r in res["hits"]["hits"]:
+        applicant = r["_source"]["applicant"]
+        if "location" in r["_source"]:
+            truck = {
+                "hours"    : r["_source"].get("dayshours", "NA"),
+                "schedule" : r["_source"].get("schedule", "NA"),
+                "address"  : r["_source"].get("address", "NA"),
+                "location" : r["_source"]["location"]
+            }
+            fooditems[applicant] = r["_source"]["fooditems"]
+            temp[applicant].append(truck)
+
+    # building up results
+    results = {"trucks": []}
+    for v in temp:
+        results["trucks"].append({
+            "name": v,
+            "fooditems": formatFooditems(fooditems[v]),
+            "branches": temp[v]
+        })
+    hits = len(results["trucks"])
+    locations = sum([len(r["branches"]) for r in results["trucks"]])
+
+    return jsonify({
+        "results": results,
+        "hits": hits,
+        "locations": locations,
+        "status": "success"
     })
-    return jsonify(res["hits"])
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/src/components/Sidebar.js
+++ static/src/components/Sidebar.js
@@ -1,12 +1,61 @@
 var React = require('react');
+var request = require('superagent');
 
 var Sidebar = React.createClass({
-    render() {
-        return <div>
-            <input type="text" placeholder="Burgers"/>
-            <p>Search to get started</p>
+  getInitialState() {
+    return { results: [], query: "" }
+  },
+  fetchResults() {
+    var results = [],
+        query = this.state.query;
+    request
+      .get('/search?q=' +  query)
+      .end(function(err, res) {
+        if (err) {
+          alert("error in fetching response");
+        }
+        else {
+          this.setState({ results: res.body });
+        }
+      }.bind(this));
+  },
+  handleSearch(e) {
+    e.preventDefault();
+    this.fetchResults();
+  },
+  onChange(e) {
+    this.setState({query: e.target.value});
+  },
+  render() {
+    var resultsCount = this.state.results.total || 0;
+    var query = this.state.query;
+    var results = this.state.results.hits || [];
+    var renderedResults = results.map((r, i) => {
+      return <li key={i}>
+          <p>{ r._source.applicant }</p>
+          <span> Located at: { r._source.address }</span>
+        </li>
+    });
+    return (
+      <div>
+        <div id="search-area">
+          <form onSubmit={this.handleSearch}>
+            <input type="text" value={query} onChange={this.onChange}
+                    placeholder="Burgers, Tacos or Wraps?"/>
+            <button>Search!</button>
+          </form>
         </div>
-    }
+        { resultsCount > 0 ?
+          <div id="results-area">
+            <h5>Showing { resultsCount } results</h5>
+            <ul>
+              { renderedResults }
+            </ul>
+          </div>
+        : null}
+      </div>
+    );
+  }
 });
 
 module.exports = Sidebar;
