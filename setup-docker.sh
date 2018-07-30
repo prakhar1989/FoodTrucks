3fc86f0496c4084f31238be4b62dbee33c51ee86
Prakhar Srivastav <prsrivastav@google.com>
2018-07-29 17:31:06 -0700

    updated docker run script with correct elasticsearch container

--- setup-docker.sh
+++ setup-docker.sh
@@ -4,10 +4,10 @@
 docker build -t prakhar1989/foodtrucks-web .
 
 # create the network
-docker network create foodtrucks
+docker network create foodtrucks-net
 
 # start the ES container
-docker run -d --net foodtrucks -p 9200:9200 -p 9300:9300 --name es elasticsearch
+docker run -d --name es --net foodtrucks-net -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
 
 # start the flask app container
-docker run -d --net foodtrucks -p 5000:5000 --name foodtrucks-web prakhar1989/foodtrucks-web
+docker run -d --net foodtrucks-net -p 5000:5000 --name foodtrucks-web prakhar1989/foodtrucks-web
