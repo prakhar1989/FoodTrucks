be0695b603fecd603b2613c89e2d9624944503dd
Prakhar Srivastav <prsrivastav@google.com>
2020-04-15 18:05:21 -0700

    fix aws ecs script with new folder structure

--- setup-aws-ecs.sh
+++ setup-aws-ecs.sh
@@ -4,10 +4,10 @@
 ecs-cli configure --region us-east-1 --cluster foodtrucks
 
 # setup cloud formation template
-ecs-cli up --keypair ecs --capability-iam --size 2 --instance-type t2.micro
+ecs-cli up --keypair ecs --capability-iam --size 1 --instance-type t2.medium
 
 # deploy
-ecs-cli compose --file aws-compose.yml up
+cd aws-ecs && ecs-cli compose --file aws-compose.yml up
 
 # check
 ecs-cli ps
